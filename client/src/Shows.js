eb5590e724a239b04068910d65a6aa8b3f121c28
Jackie Tung <jackietung3@gmail.com>
2022-05-11 11:00:27 -0400

    show

--- client/src/Shows.js
+++ client/src/Shows.js
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
+import "./Show.css"
+import ShowItem from "./ShowItem"
 
-function Shows() {
-    return null;
+function Shows({shows}) {
+    const [showList, setShowList] = useState([])
+
+
+    return (
+        <section id="show_selection">
+            <div className="wrapper">
+                <div id="btnWrapper">
+                    <a href="#showTitle">
+                        <button id="downBtn">ᐯ</button>
+                    </a>
+                </div>
+            </div>
+
+            <div className="wrapper">
+                <h2 id="showTitle">Shows</h2>
+            </div>
+
+        </section>
+    );
 }
 
 export default Shows;
